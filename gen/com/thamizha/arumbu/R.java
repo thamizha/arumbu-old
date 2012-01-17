60fe30cef86519e4dd2e355abcc82d7c0c3ae3d5
Manikk <thulir.manikk@gmail.com>
2012-01-17 09:06:27 +0530

    Basic Features Made to Work

--- gen/com/thamizha/arumbu/R.java
+++ gen/com/thamizha/arumbu/R.java
@@ -11,8 +11,9 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int arumbu_logo=0x7f020000;
+        public static final int arumbu=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int splash=0x7f020002;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/com/thamizha/arumbu/App.java
+++ src/com/thamizha/arumbu/App.java
@@ -2,12 +2,24 @@ package com.thamizha.arumbu;
 
 import com.phonegap.*;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class App extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.setIntegerProperty("splashscreen", R.drawable.splash);
         super.loadUrl("file:///android_asset/www/index.html");
     }
+    
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        
+        return false;        
+       
+    } 
 }
